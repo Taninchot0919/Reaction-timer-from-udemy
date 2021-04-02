8722fc9d14403d458148dd23e257f1acfbfdcc98
Taninchot Phuwaloetthiwat <Taninchot.art@mail.kmutt.ac.th>
2021-04-03 00:28:22 +0700

    [ADD] First Commit Init Vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <!-- disabled used to can't click button -->
+  <button @click="startPlaying" :disabled="isPlaying">Play</button>
+  <!-- v-bind blockDelay using the value of delay -->
+  <Block v-if="isPlaying" :blockDelay="delay"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay : null,
+    }
+  },
+  methods: {
+    startPlaying(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true;
+    }
+  },
 }
 </script>
 
---
+++ src/components/Block.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="block" v-if="isShowBlock">
+      Click Me
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['blockDelay'],
+    data() {
+        return {
+            isShowBlock : false
+        }
+    },
+    mounted() {
+        console.log('Block has mounted')
+        setTimeout(()=>{
+            this.isShowBlock = true;
+            console.log(this.blockDelay)
+        } ,this.blockDelay)
+    },
+    updated() {
+        console.log('updated')
+    },
+    unmounted() {
+        console.log('unmounted')
+    },
+}
+</script>
+
+<style>
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        background-color: #0faf87;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
